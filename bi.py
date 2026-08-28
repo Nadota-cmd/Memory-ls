80fdece618010675f07ab7f9458a4d8ccc1d00f8
Nadota-cmd <fishtashka78@gmail.com>
2026-08-28 05:45:39 +0500

    Create bi.py
    я чуствую как мне стыдно будет за свастон в комментах, или меня забанят

---
+++ bi.py
@@ -0,0 +1,20 @@
+import random
+format = {".sh": 5,".cpp": 40,".py": 30,".txt": 10}
+print("\033[31mПлохо\033[0m")                           g = 0
+for u in range(3):
+    memory = ["test","ai","s","as","xsv","zf","cn","kd","nf"]
+    f, w = list(format.keys()), list(format.values())
+    for uwu in range(0, len(memory)):
+        res = random.choices(f, weights=w, k=1)[0]
+        print(memory[uwu] + res)
+print("\033[32mЯ бью детей\033[0m")
+#for i in range  en(me        ):
+#    if len(mem  y[i]) > len(memory[i + 1]):
+    #    if mem              memory[i + 1]:
+   #         memory[i] mem   y[i + 1] = memory[i + 1], memory[i]
+  #          pr        ry[-  emory[1])
+ #       else:no one needs you
+#          break yes,
+nok = len(memory) - (len(memory) // 2)
+for e in range(4):
+   print(f"{memory[e] + res}     {memory[e + nok] + res}")
